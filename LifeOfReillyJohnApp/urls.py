07084333948ce04544dc6f1a8ebf5b90eac770f6
JohnReilly70 <johnreilly1989@hotmail.co.uk>
2019-05-30 21:28:43 +0100

    fixed password reset erros, sorted form styling

--- LifeOfReillyJohnApp/urls.py
+++ LifeOfReillyJohnApp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -10,27 +10,26 @@ urlpatterns = [
     path("SignUp/", views.SignUp, name="SignUp"),
     path("LogOut/", views.LogOut, name="LogOut"),
     path("LogIn/", views.LogIn, name="LogIn"),
-    path('password-reset/',
-        auth_views.PasswordResetView.as_view(
-            template_name='registration/password_reset.html'
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+            template_name='registration/password_reset.html',
+            email_template_name='registration/password_reset_email.html',
+            success_url=reverse_lazy('LifeOfReillyJohnApp:password_reset_done')
         ),
         name='password_reset'),
     
-    path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
              template_name='registration/password_reset_done.html'
          ),
          name='password_reset_done'),
 
     
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='registration/password_reset_confirm.html',
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html',
+            success_url=reverse_lazy('LifeOfReillyJohnApp:password_reset_complete')
          ),
          name='password_reset_confirm'),
     
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
              template_name='registration/password_reset_complete.html'
          ),
          name='password_reset_complete'),
