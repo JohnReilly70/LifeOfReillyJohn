f33db7a5281822fa51edc153ce3e860511188ae3
JohnReilly70 <johnreilly1989@hotmail.co.uk>
2019-05-31 23:18:14 +0100

    Created Pokemon Model & Inserted data into the DB

--- LifeOfReillyJohnApp/admin.py
+++ LifeOfReillyJohnApp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Pokemon
+from tinymce.widgets import TinyMCE
+from django.db import models
+# Register your models here.
+
+class PokemonInfoAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        ("Pokemon", {'fields': ['Poke_Number', 'Name', 'Generation', 'Legendary']}),
+	    ("Stats", {'fields': ['Type1', 'Type2', 'HP', 'Attack', 'Defence', 'SPAttack', 'SPDefence', 'Speed']})
+    ]
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+        }
+
+# Register your models here.
+admin.site.register(Pokemon, PokemonInfoAdmin)
--- LifeOfReillyJohnApp/models.py
+++ LifeOfReillyJohnApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Pokemon(models.Model):
+	
+	
+	class Meta:
+		ordering = ['Poke_Number']
+		
+		
+	Poke_Number = models.IntegerField()
+	Name = models.CharField(max_length=50)
+	Type1 = models.CharField(max_length=50)
+	Type2 = models.CharField(max_length=50)
+	HP = models.IntegerField(default=0)
+	Attack = models.IntegerField(default=0)
+	Defence = models.IntegerField(default=0)
+	SPAttack = models.IntegerField(default=0)
+	SPDefence = models.IntegerField(default=0)
+	Speed = models.IntegerField(default=0)
+	Generation = models.IntegerField(default=1)
+	Legendary = models.BooleanField(default=False)
+	
+	
+
+	
+	def __str__(self):
+		return f'{self.Poke_Number}: {self.Name}'
